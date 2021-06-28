e00745f1278d9f0a99fb319daed96c013d0b9423
Danny <mr.basilaev@gmail.com>
2021-06-28 20:07:52 +0300

    подключили бд(почти)

--- go.mod
+++ go.mod
@@ -7,14 +7,17 @@ require (
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
--- main.go
+++ main.go
@@ -1,53 +1,66 @@
 package main
 
 import (
-	"net/http"
+	"2021itmo/DB"
+	"database/sql"
+	_ "database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-import (
-	"html/template"
-	"io/ioutil"
-	"log"
-)
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-
-
-
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/"):]
-	p, _ := loadPage(title)
-	t, err := template.ParseFiles("tamplates/index.html")
-	t.Execute(w, p)
-	if err != nil {
-		log.Println(err)
-	}
+type user struct {
+	name     string
+	lastname string
+	login    string
+	password string
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("tamplates/static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", viewHandler)
+	r := gin.Default()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//регистрация нового пользователя
+	//вход пользователя в систему
+	//кодирование параши
 
+	//r.LoadHTMLGlob("tamplates/*")
+
+	//регистрация пользователя
+	r.POST("/2021itmo/tamplates/index.html", func(c *gin.Context) {
+		name := c.PostForm("name")
+		lastname := c.PostForm("lastname")
+		login := c.PostForm("login")
+		password := c.PostForm("password")
+		DB.Connect().Exec("INSERT INTO user (name, lastname, login, password) values ($1, $2, $3, $4)", name, lastname, login, password)
+		//c.JSON(200, gin.H{"new user":login})// тут вернуть страницу с пользователем
+	})
+
+	//вход пользователя в систему
+	r.GET("/2021itmo/tamplates/index.html", func(c *gin.Context) {
+		login := c.PostForm("login")
+		password := c.PostForm("password")
+		usr := DB.Connect().QueryRow("SELECT * FROM user WHERE login = $1 and password =$2", login, password)
+		us := new(user)
+		err := usr.Scan(&us.name, &us.lastname)
+		if err != sql.ErrNoRows {
+			msg := "Добро пожаловать, " + us.name + " " + us.lastname
+
+			c.JSON(200, gin.H{
+				"message": msg,
+			})
+			// возвращаем страницу пользователя
+		}
+		if err != nil {
+			msg := "неправильное имя пользователя или пароль"
+			c.JSON(404, gin.H{
+				"massage": msg,
+			})
+		}
+	})
+
+	//бработчик кнопки "кодировать"
+	/*r.POST("/", func(c *gin.Context){
+
+	})*/
+
+	r.Run()
 }
